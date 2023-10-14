cd7064f083cad145ccb3b9cc34fcd882f3762945
SimoneCarena <simonecarena2000@gmail.com>
2023-10-14 14:48:04 +0200

    Fixed window_subsystem warning

--- src/hotkey_popup.rs
+++ src/hotkey_popup.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 use global_hotkey::hotkey::HotKey;
 use keyboard_types::{Code, Modifiers};
 
